ec97bca4f5128e48499032650f1f4832fd3f27cd
seung.bok <seungbok.lee@huinnoaim.com>
2024-04-09 16:28:41 +0900

    테스트케이스 세분화하기

--- src/test/java/com/boki/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/com/boki/cafekiosk/unit/CafeKioskTest.java
@@ -5,6 +5,7 @@ import com.boki.cafekiosk.unit.beverage.Latte;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CafeKioskTest {
@@ -24,6 +25,28 @@ class CafeKioskTest {
 
         assertThat(cafeKiosk.getBeverages()).hasSize(1);
         assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void addSeveralBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano, 2);
+
+        assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+        assertThat(cafeKiosk.getBeverages().get(1)).isEqualTo(americano);
+    }
+
+    @Test
+    void addZeroBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        assertThatThrownBy(
+                () -> cafeKiosk.add(americano, 0)
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("음료는 1잔 이상 주문하실 수 없습니다.");
     }
 
     @Test
