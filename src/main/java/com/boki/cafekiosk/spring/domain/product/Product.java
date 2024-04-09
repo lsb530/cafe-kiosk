8fb3f39891dcb26d931ebc27fa3d86fa4e304377
boki <lsb530@naver.com>
2024-04-10 01:22:37 +0900

    Persistent Layer Test

--- src/main/java/com/boki/cafekiosk/spring/domain/product/Product.java
+++ src/main/java/com/boki/cafekiosk/spring/domain/product/Product.java
@@ -3,6 +3,7 @@ package com.boki.cafekiosk.spring.domain.product;
 import com.boki.cafekiosk.spring.domain.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,5 +26,14 @@ public class Product extends BaseEntity {
     private String name;
 
     private int price;
+
+    @Builder
+    public Product(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
 }
 
