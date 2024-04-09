6f20dc9bc23660fa4147a1fe972eee2f28fedf80
seung.bok <seungbok.lee@huinnoaim.com>
2024-04-09 18:54:18 +0900

    DisplayName을 섬세하게

--- src/test/java/com/boki/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/com/boki/cafekiosk/unit/CafeKioskTest.java
@@ -3,6 +3,7 @@ package com.boki.cafekiosk.unit;
 import com.boki.cafekiosk.unit.beverage.Americano;
 import com.boki.cafekiosk.unit.beverage.Latte;
 import com.boki.cafekiosk.unit.order.Order;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -21,6 +22,8 @@ class CafeKioskTest {
         System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
     }
 
+    // @DisplayName("음료 1개 추가 테스트")
+    @DisplayName("음료 1개 추가하면 주문 목록에 담긴다.") // 더 명확함
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
@@ -106,6 +109,8 @@ class CafeKioskTest {
         assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
     }
 
+    // @DisplayName("특정 시간 이전에 주문을 생성하면 실패한다.") // 테스트의 현상을 중심으로 기술하지 말 것(ex: 실패한다.)
+    @DisplayName("영업 시작 시간 이전에는 주문을 생성할 수 없다.") // 도메인 용어를 사용하여 한층 추상화된 내용을 담기(메서드 자체의 관점보다 도메인 정책 관점으로)
     @Test
     void createOrderOutsideOpenTime() {
         CafeKiosk cafeKiosk = new CafeKiosk();
