88a7b055d3154e4f83ac2f62a3a09334139646f5
boki <lsb530@naver.com>
2024-06-18 01:19:57 +0900

    refactor: Mock and InjectMocks

--- src/test/java/com/boki/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/com/boki/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -5,22 +5,31 @@ import com.boki.cafekiosk.spring.domain.history.mail.MailSendHistory;
 import com.boki.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class MailServiceTest {
+
+    @Mock
+    private MailSendClient mailSendClient;
+
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @InjectMocks
+    private MailService mailService;
 
     @DisplayName("메일 전송 테스트")
     @Test
     void sendMail() {
         // given
-        MailSendClient mailSendClient = mock(MailSendClient.class);
-        MailSendHistoryRepository mailSendHistoryRepository = mock(MailSendHistoryRepository.class);
-
-        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
-
         when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
                 .thenReturn(true);
 
