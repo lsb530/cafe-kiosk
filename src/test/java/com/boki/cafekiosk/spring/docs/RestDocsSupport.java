34be7592e152be9708d82a9a16cf926565bdaab8
boki <lsb530@naver.com>
2024-06-23 13:09:53 +0900

    add RestDocsSupport

---
+++ src/test/java/com/boki/cafekiosk/spring/docs/RestDocsSupport.java
@@ -0,0 +1,27 @@
+package com.boki.cafekiosk.spring.docs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class RestDocsSupport {
+
+    protected MockMvc mockMvc;
+    protected ObjectMapper objectMapper = new ObjectMapper();
+
+    protected abstract Object initController();
+
+    @BeforeEach
+    void setUp(RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(initController()) // 인자로 컨트롤러를 넣는다
+            .apply(documentationConfiguration(provider))
+            .build();
+    }
+}
